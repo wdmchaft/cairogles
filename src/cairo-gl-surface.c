aff05620e66e6f0289e01ca1610bc6d88af57717
Henry Song <henry.song@samsung.com>
2012-06-04 14:36:26 +0200

    gl/msaa: Add image "atlas" optimization.
    In gl, we create a image atlas.  When an gl surface is to be painted to
    target surface, we check whether the image can be cached into the atlas.
    Using the image atlas, we can avoid flushing and rebinding textures of multiple
    images if they are cached in the atlas.
    
    For this version, we don't cache other backend surface yet.

--- src/cairo-gl-surface.c
+++ src/cairo-gl-surface.c
@@ -391,6 +391,7 @@ _cairo_gl_surface_init (cairo_device_t *device,
     surface->width = width;
     surface->height = height;
     surface->needs_update = FALSE;
+    surface->needs_to_cache = FALSE;
 
     _cairo_gl_surface_embedded_operand_init (surface);
 }
@@ -515,6 +516,7 @@ _cairo_gl_surface_clear (cairo_gl_surface_t  *surface,
     if (a == 0)
 	surface->base.is_clear = TRUE;
 
+    surface->content_changed = TRUE;
     return _cairo_gl_context_release (ctx, status);
 }
 
@@ -965,6 +967,8 @@ FAIL:
     if (clone)
         cairo_surface_destroy (&clone->base);
 
+    if (likely (status))
+	dst->content_changed = TRUE;
     return status;
 }
 
@@ -1270,6 +1274,9 @@ _cairo_gl_surface_paint (void			*surface,
 			 const cairo_pattern_t	*source,
 			 const cairo_clip_t	*clip)
 {
+    cairo_int_status_t status;
+    cairo_gl_surface_t *dst = (cairo_gl_surface_t *)surface;
+
     /* simplify the common case of clearing the surface */
     if (clip == NULL) {
         if (op == CAIRO_OPERATOR_CLEAR)
@@ -1282,8 +1289,11 @@ _cairo_gl_surface_paint (void			*surface,
         }
     }
 
-    return _cairo_compositor_paint (get_compositor (surface), surface,
-				    op, source, clip);
+    status = _cairo_compositor_paint (get_compositor (surface), surface,
+				      op, source, clip);
+    if (likely (status))
+	dst->content_changed = TRUE;
+    return status;
 }
 
 static cairo_int_status_t
@@ -1293,8 +1303,14 @@ _cairo_gl_surface_mask (void			 *surface,
 			const cairo_pattern_t	*mask,
 			const cairo_clip_t	*clip)
 {
-    return _cairo_compositor_mask (get_compositor (surface), surface,
-				   op, source, mask, clip);
+    cairo_int_status_t status;
+    cairo_gl_surface_t *dst = (cairo_gl_surface_t *) surface;
+
+    status = _cairo_compositor_mask (get_compositor (surface), surface,
+				     op, source, mask, clip);
+    if (likely (status))
+	dst->content_changed = TRUE;
+    return status;
 }
 
 static cairo_int_status_t
@@ -1309,10 +1325,16 @@ _cairo_gl_surface_stroke (void			        *surface,
                           cairo_antialias_t		 antialias,
                           const cairo_clip_t		*clip)
 {
-    return _cairo_compositor_stroke (get_compositor (surface), surface,
-				     op, source, path, style,
-				     ctm, ctm_inverse, tolerance, antialias,
-				     clip);
+    cairo_int_status_t status;
+    cairo_gl_surface_t *dst = (cairo_gl_surface_t *)surface;
+
+    status = _cairo_compositor_stroke (get_compositor (surface), surface,
+				       op, source, path, style,
+				       ctm, ctm_inverse, tolerance,
+				       antialias, clip);
+    if (likely (status))
+	dst->content_changed = TRUE;
+    return status;
 }
 
 static cairo_int_status_t
@@ -1325,10 +1347,16 @@ _cairo_gl_surface_fill (void			*surface,
                         cairo_antialias_t	 antialias,
                         const cairo_clip_t	*clip)
 {
-    return _cairo_compositor_fill (get_compositor (surface), surface,
-				   op, source, path,
-				   fill_rule, tolerance, antialias,
-				   clip);
+    cairo_int_status_t status;
+    cairo_gl_surface_t *dst = (cairo_gl_surface_t *)surface;
+
+    status = _cairo_compositor_fill (get_compositor (surface), surface,
+				     op, source, path,
+				     fill_rule, tolerance, antialias,
+				     clip);
+    if (likely (status))
+	dst->content_changed = TRUE;
+    return status;
 }
 
 static cairo_int_status_t
@@ -1340,9 +1368,15 @@ _cairo_gl_surface_glyphs (void			*surface,
 			  cairo_scaled_font_t	*font,
 			  const cairo_clip_t	*clip)
 {
-    return _cairo_compositor_glyphs (get_compositor (surface), surface,
-				     op, source, glyphs, num_glyphs, font,
-				     clip);
+    cairo_int_status_t status;
+    cairo_gl_surface_t *dst = (cairo_gl_surface_t *)surface;
+
+    status = _cairo_compositor_glyphs (get_compositor (surface), surface,
+				       op, source, glyphs, num_glyphs,
+				       font, clip);
+    if (likely (status))
+	dst->content_changed = TRUE;
+    return status;
 }
 
 static const cairo_surface_backend_t _cairo_gl_surface_backend = {
